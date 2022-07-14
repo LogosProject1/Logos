cb137b75473106233b51ea27c08ee34067899425
Blue-Bug <kjj9987@gmail.com>
2022-07-14 15:52:48 +0900

    knowledgeDto 생성

--- BackEnd/knowledge/src/main/java/com/logos/knowledge/dto/KnowledgeDto.java
+++ BackEnd/knowledge/src/main/java/com/logos/knowledge/dto/KnowledgeDto.java
@@ -2,13 +2,20 @@ package com.logos.knowledge.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class KnowledgeDto {
     private String title;
 
-    private List<String> category;
+    private String category;
 
+    private String content;
 
+    private String price;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
 }
