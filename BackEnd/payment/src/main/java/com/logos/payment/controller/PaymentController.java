ed1c1ed6a0d911b96f0116961b571c162e37984e
Blue-Bug <kjj9987@gmail.com>
2022-07-30 15:38:50 +0900

    포인트 충전, 결제 내역 확인 구현

--- BackEnd/payment/src/main/java/com/logos/payment/controller/PaymentController.java
+++ BackEnd/payment/src/main/java/com/logos/payment/controller/PaymentController.java
@@ -1,18 +1,17 @@
 package com.logos.payment.controller;
 
 import com.logos.payment.dto.OrderDto;
+import com.logos.payment.dto.PaymentHistoryDto;
 import com.logos.payment.dto.VerifyDto;
 import com.logos.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,10 +26,10 @@ public class PaymentController {
         Map<String,Object> resultMap = new HashMap<>();
         HttpStatus status = null;
 
-        String email = (String) req.getAttribute("email");
+        String email = (String) req.getAttribute("Email");
         try {
             String orderId = paymentService.makeOrder(email, orderDto);
-            resultMap.put("order_id",orderId);
+            resultMap.put("merchant_uid",orderId);
             status = HttpStatus.OK;
         }catch (Exception e){
             resultMap.put("message",e.getMessage());
@@ -45,9 +44,27 @@ public class PaymentController {
         Map<String,Object> resultMap = new HashMap<>();
         HttpStatus status = null;
 
-        String email = (String) req.getAttribute("email");
+        String email = (String) req.getAttribute("Email");
         try {
             String result = paymentService.verifyOrder(email, verifyDto);
+            resultMap.put("result", result);
+            status = HttpStatus.OK;
+        }catch (Exception e){
+            resultMap.put("message",e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<>(resultMap,status);
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<Map<String,Object>> verifyOrder(HttpServletRequest req){
+        Map<String,Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+
+        String email = (String) req.getAttribute("Email");
+        try {
+            List<PaymentHistoryDto> result = paymentService.getPaymentHistory(email);
             resultMap.put("result", result);
             status = HttpStatus.OK;
         }catch (Exception e){
--- BackEnd/payment/src/main/java/com/logos/payment/repository/PaymentRepository.java
+++ BackEnd/payment/src/main/java/com/logos/payment/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.logos.payment.repository;
 import com.logos.payment.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment,String> {
+    List<Payment> findByUserEmail(String email);
 }
