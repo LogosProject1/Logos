6c7684fbf0e4b8f4a05ae194183b0bf545ab39b3
Blue-Bug <kjj9987@gmail.com>
2022-08-17 14:26:38 +0900

    지식 검색 구현 완료

--- FrontEnd/logos-front/src/components/knowledge/SearchComponent.vue
+++ FrontEnd/logos-front/src/components/knowledge/SearchComponent.vue
@@ -5,13 +5,17 @@
     <b-row>
       <b-card-group columns>
         <b-card
-          title="제목"
+          v-for="knowledge in knowledgeList"
+          :key="knowledge.id"
+          :title="knowledge.title"
           img-src="https://picsum.photos/300/200/?image=40"
           img-alt="Image"
           img-top
         >
           <b-card-text>
-            애옹이
+            <p>가격 : {{ knowledge.price }} 포인트</p>
+            <p>시작 시간 : {{ knowledge.startTime | moment("LLLL") }}</p>
+            <p>종료 시간 : {{ knowledge.endTime | moment("LLLL") }}</p>
             <b-row class="mt-3">
               <div class="col text-center">
                 <b-button variant="primary" float>상세보기</b-button>
@@ -19,102 +23,58 @@
             </b-row>
           </b-card-text>
           <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </template>
-        </b-card>
-
-        <b-card
-          title="제목"
-          img-src="https://picsum.photos/300/200/?image=41"
-          img-alt="Image"
-          img-top
-        >
-          <b-card-text>
-            빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울빗방울
-            <b-row class="mt-3">
-              <div class="col text-center">
-                <b-button variant="primary" float>상세보기</b-button>
-              </div>
-            </b-row>
-          </b-card-text>
-          <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </template>
-        </b-card>
-
-        <b-card
-          title="Title"
-          img-src="https://picsum.photos/300/200/?image=42"
-          img-alt="Image"
-          img-top
-        >
-          <b-card-text>
-            100자 제한
-            <b-row class="mt-3">
-              <div class="col text-center">
-                <b-button variant="primary" float>상세보기</b-button>
-              </div>
-            </b-row>
-          </b-card-text>
-          <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </template>
-        </b-card>
-        <b-card
-          title="Title"
-          img-src="https://picsum.photos/300/200/?image=42"
-          img-alt="Image"
-          img-top
-        >
-          <b-card-text>
-            가장 긴 텍스트에 높이가 맞춰짐
-            <b-row class="mt-3">
-              <div class="col text-center">
-                <b-button variant="primary" float>상세보기</b-button>
-              </div>
-            </b-row>
-          </b-card-text>
-          <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </template>
-        </b-card>
-        <b-card
-          title="Title"
-          img-src="https://picsum.photos/300/200/?image=42"
-          img-alt="Image"
-          img-top
-        >
-          <b-card-text>
-            가장 긴 텍스트에 높이가 맞춰짐
-            <b-row class="mt-3">
-              <div class="col text-center">
-                <b-button variant="primary" float>상세보기</b-button>
-              </div>
-            </b-row>
-          </b-card-text>
-          <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </template>
-        </b-card>
-        <b-card
-          title="Title"
-          img-src="https://picsum.photos/300/200/?image=42"
-          img-alt="Image"
-          img-top
-        >
-          <b-card-text>
-            가장 긴 텍스트에 높이가 맞춰짐
-            <b-row class="mt-3">
-              <div class="col text-center">
-                <b-button variant="primary" float>상세보기</b-button>
-              </div>
-            </b-row>
-          </b-card-text>
-          <template #footer>
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted"
+              >{{ knowledge.updated_at | moment("from", "now", true) }} 전에
+              업데이트 됨</small
+            >
           </template>
         </b-card>
       </b-card-group>
     </b-row>
   </b-container>
 </template>
+
+<script>
+import { filterKnowledge } from "@/api/knowledge";
+export default {
+  data() {
+    return {
+      knowledgeList: [],
+      category: this.$route.query.category,
+    };
+  },
+  updated() {
+    this.$nextTick(function () {
+      this.category = this.$route.query.category;
+      console.log("updated 호출 " + this.category);
+    });
+  },
+  watch: {
+    category: function () {
+      console.log("watch 호출");
+      filterKnowledge(
+        { params: { category: this.category } },
+        (response) => {
+          console.log("카테고리 가져오기 완료");
+          this.knowledgeList = response.data.knowledge_list;
+        },
+        () => {
+          console.log("카테고리 가져오기 실패");
+        }
+      );
+    },
+  },
+  mounted() {
+    filterKnowledge(
+      { params: { category: this.category } },
+      (response) => {
+        console.log("카테고리 가져오기 완료");
+        this.knowledgeList = response.data.knowledge_list;
+      },
+      () => {
+        console.log("카테고리 가져오기 실패");
+      }
+    );
+  },
+};
+</script>
