9a1dbe39f044f6e8b9817b7c381138f5a0110c88
Blue-Bug <kjj9987@gmail.com>
2022-08-23 14:56:22 +0900

    에디터 구현 중

--- FrontEnd/logos-front/src/views/KnowledgeCreateView.vue
+++ FrontEnd/logos-front/src/views/KnowledgeCreateView.vue
@@ -1,12 +1,54 @@
 <template>
-  <tip-tap />
+  <b-container>
+    <b-input-group prepend="제목" class="mb-2">
+      <b-form-input aria-label="title"></b-form-input>
+    </b-input-group>
+    <div id="date">
+      <b-input-group prepend="시작 날짜" class="mb-2">
+        <date-range-picker
+          v-model="dateRange"
+          :timePicker="true"
+          :timePicker24Hour="true"
+          opens="right"
+        >
+          <template v-slot:input="picker">
+            {{ picker.startDate | moment("YYYY-MM-DD HH:mm") }} - <br />{{
+              picker.endDate | moment("YYYY-MM-DD HH:mm")
+            }}
+          </template>
+        </date-range-picker>
+      </b-input-group>
+    </div>
+    <editor
+      :options="editorOptions"
+      height="700px"
+      initialEditType="wysiwyg"
+      previewStyle="vertical"
+    />
+    <b-button class="mt-3 mb-3 float-right" variant="primary"
+      >생성하기</b-button
+    >
+  </b-container>
 </template>
 <script>
-import TipTap from "@/components/knowledge/TipTap.vue";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
+import DateRangePicker from "vue2-daterange-picker";
+import { Editor } from "@toast-ui/vue-editor";
 
 export default {
-  components: {
-    TipTap,
+  components: { editor: Editor, DateRangePicker },
+  data() {
+    return {
+      dateRange: {
+        startDate: undefined,
+        endDate: undefined,
+      },
+      editorOptions: {
+        hideModeSwitch: true,
+      },
+    };
   },
 };
 </script>
+<style></style>
